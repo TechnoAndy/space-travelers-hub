d9b4b23f17105849185c9b7b7f33f4a3dfa2dfe8
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-11-29 20:06:45 +0200

    Display reserved items on my profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,32 @@
 import React from 'react';
-/* import { Link } from 'react-router-dom'; */
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const Profile = () => (
-  <div className="container">
-    {/* <div className="profile">
-      <Link>Make Reservations</Link>
-    </div> */}
-    <h1>Moving on...</h1>
-  </div>
-);
+const Profile = () => {
+  const { rockets } = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocks) => rocks.active);
+
+  return (
+    <div className="profile">
+      <div className="rockets">
+        <h3>My Rockets</h3>
+        {reservedRockets.length === 0 ? (
+          <div className="noReservations">
+            <p>No Reservations Made</p>
+            <Link className="reserveRocketsLink" to="/">Make Reservations</Link>
+          </div>
+        ) : (
+          <div className="activeRockets">
+            <ul>
+              {reservedRockets.map((rocket) => (
+                <li key={rocket.id}>{rocket.rocket_name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
