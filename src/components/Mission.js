33571cdff78f8ab79bbfcc7aaf184ca7aae56f4d
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2022-12-02 14:43:14 +0100

    Configure store

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,7 +1,25 @@
-const Mission = () => (
-  <div className="container">
-    <h1>Mission Status</h1>
-  </div>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missions';
+import MissionTab from '../redux/missions/MissionTab';
+
+const Mission = () => {
+  const { missions, status } = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (status === null) {
+      dispatch(fetchMissions);
+    }
+  }, [status, dispatch]);
+
+  return (
+    <div className="container">
+      {status === 'pending' && <p className="loading">Loading...</p>}
+      {status === 'rejected' && <p className="notRight">Ouch! Something is not right. Try again later.</p>}
+      {status === 'success' && <MissionTab missions={missions} />}
+    </div>
+  );
+};
 
 export default Mission;
